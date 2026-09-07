d19f93f722bca12a68d899fa34024b516867bb70
26LP86330826 <26LP86330826@Setups-iMac.local>
2026-09-07 10:58:16 +0800

    back fixed

--- food-back/src/index.js
+++ food-back/src/index.js
@@ -11,7 +11,6 @@ import { userRouter } from "./routes/user.js";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const port = 8000;
 const app = express();
 
 app.use(express.json());
@@ -22,18 +21,14 @@ app.use("/food", foodRouter);
 app.use("/order", orderRouter);
 app.use("/user", userRouter);
 
-async function startServer() {
-  await mongoose
-    .connect(
-      "mongodb+srv://irmuungantulga2_db_user:UansysmMozwk6Nyd@cluster0.2x10ogk.mongodb.net/",
-    )
-    .catch((err) => console.error("MongoDB connection error:", err));
-  console.log("Connected ");
-  if (!process.env.VERCEL) {
-    const port = 8000;
-    app.listen(port, () => {
-      console.log(`server is running on http://localhost:${port}`);
-    });
-  }
+await mongoose.connect(process.env.MONGODB_URI);
+console.log("Connected ");
+
+if (!process.env.VERCEL) {
+  const port = 8000;
+  app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+  });
 }
-startServer();
+
+export default app;
